425912ce5678757fc01bcd0b40bf5dcafd283282
= <ishannepal82@gmail.com>
2026-05-08 18:12:27 +0545

    fixed

--- frontend/src/hooks/api.ts
+++ frontend/src/hooks/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useAuthStore } from '../store/authStore'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8787/api',
+  baseURL: "https://hyper_revise.ishannepal82.workers.dev/api",
   headers: {
     'Content-Type': 'application/json',
   },
