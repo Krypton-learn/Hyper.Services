81ea139dd2a054042f1cdfc10e2da4920a3d0661
Ishan Nepal <ishannepal82@gmail.com>
2026-05-14 10:54:09 +0545

    Change API base URL to Arcademia

--- frontend/src/hooks/api.ts
+++ frontend/src/hooks/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useAuthStore } from '../store/authStore'
 
 const api = axios.create({
-  baseURL: "https://hyper_revise.ishannepal82.workers.dev/api",
+  baseURL: "https://api.arcademia.app/api",
   headers: {
     'Content-Type': 'application/json',
   },
